cf9fca827481dd6b4a84d301fa6bb9685786ee7c
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-07-15 12:31:36 -0300

    commit

---
+++ Functions/FoFiHelloWorld.cs
@@ -0,0 +1,70 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Logging;
+
+namespace DemoFunctionApp.Functions
+{
+    public static class FoFiDurableFunctions
+    {
+        [Function(nameof(FoFiDurableFunctions))]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] TaskOrchestrationContext context)
+        {
+            ILogger logger = context.CreateReplaySafeLogger(nameof(FoFiDurableFunctions));
+            logger.LogInformation("Saying hello.");
+            var outputs = new List<string>();
+
+            // Replace name and input with values relevant for your Durable Functions Activity
+            var argentinaTask = context.CallActivityAsync<string>(nameof(FoFiSayHelloActivity), "Argentina");
+            var peruTask = context.CallActivityAsync<string>(nameof(FoFiSayHelloActivity), "Peru");
+            var mexicoTask = context.CallActivityAsync<string>(nameof(FoFiSayHelloActivity), "Mexico");
+
+            // Esperar a que todas las tareas se completen
+            var results = await Task.WhenAll(argentinaTask, peruTask, mexicoTask);
+
+            // Agregar los resultados a la lista outputs
+            outputs.AddRange(results);
+
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
+            return outputs;
+        }
+
+        [Function(nameof(FoFiSayHelloActivity))]
+        public static string FoFiSayHelloActivity([ActivityTrigger] string name, FunctionContext executionContext)
+        {
+            ILogger logger = executionContext.GetLogger("SayHello");
+
+            // Generar un tiempo de espera aleatorio entre 15 y 45 segundos
+            Random random = new Random();
+            int delay = random.Next(30000, 45000);
+            logger.LogInformation("Saying hello to {name} in {delay}.", name, delay);
+
+            // Hacer que el hilo duerma durante el tiempo generado
+            Thread.Sleep(delay);
+
+            logger.LogInformation("Saying hello to {name}.", name);
+            return $"Hello {name}!";
+        }
+
+        [Function($"{nameof(FoFiDurableFunctions)}_Start")]
+        public static async Task<HttpResponseData> FoFiHttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+            [DurableClient] DurableTaskClient client,
+            FunctionContext executionContext)
+        {
+            ILogger logger = executionContext.GetLogger($"{nameof(FoFiDurableFunctions)}_Start");
+
+            // Function input comes from the request content.
+            string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(
+                nameof(FoFiDurableFunctions));
+
+            logger.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
+
+            // Returns an HTTP 202 response with an instance management payload.
+            // See https://learn.microsoft.com/azure/azure-functions/durable/durable-functions-http-api#start-orchestration
+            return await client.CreateCheckStatusResponseAsync(req, instanceId);
+        }
+    }
+}
--- Functions/HelloWorld.cs
+++ Functions/HelloWorld.cs
@@ -1,6 +1,54 @@
-namespace DemoFunctionApp.Functions;
+using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Tree;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
-public class HelloWorld
+namespace DemoFunctionApp.Functions
 {
-    
+    public class HelloWorldFunctions(ILogger<HelloWorldFunctions> logger)
+    {
+        [Function("HttpHelloWorldHttp")]
+        public IActionResult RunHttp([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+        {
+            logger.LogInformation("HelloWorld function was triggered.");
+
+            var nameQuery = req.Query["name"].ToString();
+            var name = string.IsNullOrEmpty(nameQuery) ? "there" : nameQuery;
+            var message = $"Hi {name}! Welcome to Azure Functions!";
+
+            return new OkObjectResult(message);
+        }
+
+        [Function("HttpHelloWorldBlobStorage")]
+        [BlobOutput("hello-world/message-from-{fileName}.txt", Connection = "AZUREBLOBSTORAGE_CONFIG")]
+        public string RunStorage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+        {
+            logger.LogInformation("HelloWorldStorage function was triggered.");
+
+            var nameQuery = req.Query["name"].ToString();
+            var name = string.IsNullOrEmpty(nameQuery) ? "there" : nameQuery;
+            var message = $"Hi {name}! Welcome to Azure Functions!";
+
+            return message;
+        }
+
+        /**
+         * "* * * * *" -> "minutos horas dia_del_mes mes dia_de_la_semana"
+         * "* * * * * *" -> "segundos minutos horas dia_del_mes mes dia_de_la_semana"
+         **/
+        [Function("TimerHelloWorldBlobStorage")]
+        [BlobOutput("hello-world/timer.txt", Connection = "AZUREBLOBSTORAGE_CONFIG")]
+        public string RunTimer([TimerTrigger("0 */1 * * *")] TimerInfo myTimer)
+        {
+            logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var outName = DateTime.Now.ToString("HH") + "-" + DateTime.Now.ToString("mm") + "-timer";
+            var message = $"La hora es {outName}";
+
+            return message;
+        }
+    }
 }
